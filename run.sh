8221ebba11f6d5318fb2315f8bbe4efde9673848
Ankit-Gupta-11 <ag32734@gmail.com>
2022-06-20 18:24:39 +0530

    Updated The file structure of the project

---
+++ run.sh
@@ -0,0 +1,5 @@
+python ./training.py --fold 0 --model xgboost   
+python ./training.py --fold 1 --model xgboost
+python ./training.py --fold 2 --model xgboost
+python ./training.py --fold 3 --model xgboost
+python ./training.py --fold 4 --model xgboost
